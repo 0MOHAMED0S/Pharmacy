6a8529e20739534446da83bb0c46a015633458d8
mohamed <msa0back@gmail.com>
2025-02-20 13:40:15 +0200

    save

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'auth.pharmacy' => \App\Http\Middleware\PharmacyAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
