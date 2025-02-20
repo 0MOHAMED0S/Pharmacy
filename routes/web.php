b93d0f8e5c2bf29f48ad9069caedb1ac06565c07
mohamed <msa0back@gmail.com>
2025-02-20 12:33:55 +0200

    save

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth:pharmacy')->group(function () {
+Route::middleware('auth.pharmacy')->group(function () {
     // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/pharmacy/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -23,7 +23,7 @@ Route::middleware('auth:pharmacy')->group(function () {
 // GoogleLoginController redirect and callback urls
 Route::get('/login/google', [PharmacyGoogleController::class, 'redirectToGoogle'])->name('auth.google');
 Route::get('/login/google/callback', [PharmacyGoogleController::class, 'handleGoogleCallback']);
-Route::post('/pharmacy/logout', [PharmacyGoogleController::class, 'logout'])->name('pharmacy.logout');
+Route::post('/pharmacy/logout', [PharmacyGoogleController::class, 'logout'])->middleware('auth.pharmacy')->name('pharmacy.logout');
 
 Route::resource('medicines',MedicineController::class)->middleware('auth.pharmacy');
 Route::get('/pharmacy/profile', [PharmacyProfileController::class, 'index'])->middleware('auth.pharmacy')->name('profile.index');
